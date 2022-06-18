929070d0b352ff85eea98b881d56580150fcc8a3
Nicarex <my.profile.protect@gmail.com>
2022-06-19 01:02:01 +0300

    Add option - file

--- sending_messages.py
+++ sending_messages.py
@@ -6,12 +6,15 @@ import argparse
 import asyncio
 
 parser = argparse.ArgumentParser(description='Sending messages to email, VK and telegram')
-parser.add_argument('--message', dest='message', type=str, help='Message that will be sent.\n--message "test message"')
+parser.add_argument('--message-file', dest='message', type=str, help='Message from file that will be sent.\n--message-file "file.txt"')
 parser.add_argument('--platform', dest='platform', type=str, help='Platform to be used.\nVK: --platform "vk", email: --platform "email", telegram: --platform "telegram", all: --platform "all"')
 
 args = parser.parse_args()
 
-logger.log('SENDING', f'Run "sending_messages.py", args: message - <{args.message}>, platform - <{args.platform}>')
+logger.log('SENDING', f'Run "sending_messages.py", args: message-file - <{args.message}>, platform - <{args.platform}>')
+
+with open(args.message, 'r', encoding='utf-8') as f:
+    message = f.read()
 
 # Рассылка сообщений в почту
 if args.message is not None and (args.platform == 'all' or args.platform == 'email'):
@@ -23,7 +26,7 @@ if args.message is not None and (args.platform == 'all' or args.platform == 'ema
     c.close()
     conn.close()
     for user in users:
-        sendMail(to_email=user['email'], subject='Информирование об изменениях', text=args.message)
+        sendMail(to_email=user['email'], subject='Информирование об изменениях', text=message)
     logger.log('SENDING', 'Email finished')
 
 # Рассылка сообщений в ВК
@@ -37,9 +40,9 @@ if args.message is not None and (args.platform == 'all' or args.platform == 'vk'
     c.close()
     conn.close()
     for user in users:
-        asyncio.run(write_msg_vk_user(message=args.message, user_id=user['vk_id']))
+        asyncio.run(write_msg_vk_user(message=message, user_id=user['vk_id']))
     for chat in chats:
-        asyncio.run(write_msg_vk_chat(message=args.message, chat_id=chat['vk_id']))
+        asyncio.run(write_msg_vk_chat(message=message, chat_id=chat['vk_id']))
     logger.log('SENDING', 'VK finished')
 
 # Рассылка сообщений в Telegram
@@ -54,5 +57,5 @@ if args.message is not None and (args.platform == 'all' or args.platform == 'tel
     list_ids = []
     for user in users:
         list_ids += [user['telegram_id']]
-    asyncio.run(write_msg_telegram(message=args.message, tg_id=list_ids))
+    asyncio.run(write_msg_telegram(message=message, tg_id=list_ids))
     logger.log('SENDING', 'Telegram finished')
